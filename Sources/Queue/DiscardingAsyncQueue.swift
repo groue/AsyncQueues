0d06ee425a1300b093ba203fcdaa8ee1745d000a
Gwendal Roué <gr@pierlis.com>
2025-06-05 17:56:18 +0200

    DiscardingAsyncQueue

---
+++ Sources/Queue/DiscardingAsyncQueue.swift
@@ -0,0 +1,88 @@
+/// A queue that serializes async operations and discards cancelled ones.
+///
+/// Usage:
+///
+/// ```swift
+/// let queue = DiscardingAsyncQueue()
+///
+/// // `perform(operation:)` returns the result of the async operation.
+/// // The operation is cancelled if the current task is cancelled.
+/// let value = try await queue.perform {
+///     try await someValue()
+/// }
+///
+/// // `addTask` returns a new unstructured task that executes the
+/// // async operation.
+/// queue.addTask {
+///     try await doSomething()
+/// }
+/// ```
+public struct DiscardingAsyncQueue: Sendable {
+    private let primitiveQueue = PrimitiveAsyncQueue()
+    
+    public init() { }
+    
+    /// Returns the result of the given operation.
+    ///
+    /// The `operation` closure runs after previously enqueued operations
+    /// are completed.
+    ///
+    /// If the current task is cancelled before the previously enqueued
+    /// operations complete, `operation` is not executed and this method
+    /// throws a `CancellationError`. The operation is
+    /// considered "discarded". Otherwise, `operation` can check
+    /// `Task.isCancelled` or `Task.checkCancellation()` to
+    /// detect cancellation.
+    ///
+    /// - Parameter operation: The operation to perform.
+    /// - Returns: The result of `operation`.
+    /// - Throws: The error of `operation`, or `CancellationError` if the
+    ///   current task is cancelled before the previously enqueued
+    ///   operations complete.
+    public func perform<Success>(
+        @_inheritActorContext @_implicitSelfCapture operation: () async throws -> sending Success
+    ) async throws -> sending Success {
+        let (start, end) = primitiveQueue.makeSemaphores()
+        defer { end.signal() }
+        
+        // Stop waiting if task is cancelled.
+        try await start.waitUnlessCancelled()
+        
+        return try await operation()
+    }
+    
+    /// Returns an unstructured Task that runs the given operation.
+    ///
+    /// If the returned task is cancelled before the previously enqueued
+    /// operations complete, `operation` is not executed and the task
+    /// throws a `CancellationError`. The operation is
+    /// considered "discarded". Otherwise, `operation` can check
+    /// `Task.isCancelled` or `Task.checkCancellation()` to
+    /// detect cancellation.
+    ///
+    /// You need to keep a reference to the task if you want to cancel it
+    /// by calling the `Task.cancel()` method. Discarding your reference to
+    /// the task doesn’t implicitly cancel that task, it only makes it
+    /// impossible for you to explicitly cancel the task.
+    ///
+    /// - Parameter operation: The operation to perform.
+    /// - Returns: a Task that executes `operation`.
+    @discardableResult
+    public func addTask<Success>(
+        @_inheritActorContext @_implicitSelfCapture operation: sending @escaping () async throws -> Success
+    ) -> Task<Success, any Error> {
+        // Compiler does not see that operation is only called once.
+        typealias SendableOperation = @Sendable () async throws -> Success
+        let operation = unsafeBitCast(operation, to: SendableOperation.self)
+        
+        let (start, end) = primitiveQueue.makeSemaphores()
+        return Task {
+            defer { end.signal() }
+            
+            // Stop waiting if task is cancelled.
+            try await start.waitUnlessCancelled()
+            
+            return try await operation()
+        }
+    }
+}
---
+++ Tests/QueueTests/DiscardingAsyncQueueTests.swift
@@ -0,0 +1,315 @@
+import Testing
+@testable import Queue
+
+@Suite(.timeLimit(.minutes(1)))
+struct DiscardingAsyncQueueTests {
+    @TaskLocal static var local: Int? = nil
+    
+    // MARK: - Current Task
+    
+    @Test
+    func perform_executes_operation_in_the_current_task() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        let task = Task {
+            try await queue.perform {
+                withUnsafeCurrentTask { currentTask in
+                    currentTask?.cancel()
+                }
+                #expect(Task.isCancelled)
+            }
+            #expect(Task.isCancelled)
+        }
+        
+        try await task.value
+    }
+    
+    @Test
+    func perform_inherits_task_locals() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        let number = Int.random(in: 0..<1000)
+        try await Self.$local.withValue(number) {
+            try await queue.perform {
+                #expect(Self.local == number)
+            }
+        }
+    }
+    
+    // MARK: - Value and Error
+    
+    @Test
+    func perform_returns_operation_value() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        for value in 0..<10 {
+            let result = try await queue.perform {
+                return value
+            }
+            #expect(result == value)
+        }
+    }
+    
+    @Test
+    func perform_rethrows_thrown_error() async throws {
+        struct TestError: Error { }
+        let queue = DiscardingAsyncQueue()
+        
+        await #expect(throws: TestError.self) {
+            try await queue.perform {
+                throw TestError()
+            }
+        }
+    }
+    
+    @Test
+    func added_task_returns_operation_value() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        for value in 0..<10 {
+            let task = queue.addTask {
+                return value
+            }
+            let result = try await task.value
+            #expect(result == value)
+        }
+    }
+    
+    @Test
+    func added_task_rethrows_thrown_error() async throws {
+        struct TestError: Error { }
+        let queue = DiscardingAsyncQueue()
+        
+        let task = queue.addTask {
+            throw TestError()
+        }
+        
+        await #expect(throws: TestError.self) {
+            try await task.value
+        }
+    }
+    
+    // MARK: - Ordering
+    
+    @Test
+    func perform_is_ordered_with_task() async throws {
+        let valuesMutex = Mutex<[Int]>([])
+        let queue = DiscardingAsyncQueue()
+        for i in 0...5000 {
+            queue.addTask {
+                valuesMutex.withLock { $0.append(i * 2) }
+            }
+            try await queue.perform {
+                valuesMutex.withLock { $0.append(i * 2 + 1) }
+            }
+        }
+        let values = try await queue.perform {
+            valuesMutex.withLock { $0 }
+        }
+        #expect(values == Array(0...10001))
+    }
+    
+    @Test
+    func tasks_are_ordered() async throws {
+        let valuesMutex = Mutex<[Int]>([])
+        let queue = DiscardingAsyncQueue()
+        for i in 0...10000 {
+            queue.addTask {
+                valuesMutex.withLock { $0.append(i) }
+            }
+        }
+        let values = try await queue.perform {
+            valuesMutex.withLock { $0 }
+        }
+        #expect(values == Array(0...10000))
+    }
+    
+    @Test
+    func cancelled_tasks_are_ordered() async throws {
+        let valuesMutex = Mutex<[Int]>([])
+        let queue = DiscardingAsyncQueue()
+        for i in 0...10000 {
+            let task = queue.addTask {
+                valuesMutex.withLock { $0.append(i) }
+            }
+            if Bool.random() {
+                task.cancel()
+            }
+        }
+        let values = try await queue.perform {
+            valuesMutex.withLock { $0 }
+        }
+        #expect(values.count > 1000)
+        #expect(values == values.sorted())
+    }
+    
+    // MARK: - Cancellation
+    
+    @Test
+    func operation_is_cancelled_by_late_wrapper_task_cancellation() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        // Semaphore-like stream: signal is `continuation.finish()`,
+        // wait is `for await _ in stream { }`
+        let (didStartStream, didStartContinuation) = AsyncStream.makeStream(of: Never.self)
+        
+        let wrapperTask = Task {
+            try await queue.perform {
+                didStartContinuation.finish()
+                
+                // Wait until task is cancelled.
+                let didCancelStream = AsyncStream<Never> { _ in }
+                for await _ in didCancelStream { }
+                #expect(Task.isCancelled)
+            }
+        }
+        
+        for await _ in didStartStream { }
+        wrapperTask.cancel()
+        try await wrapperTask.value
+    }
+    
+    @Test
+    func operation_is_cancelled_by_early_wrapper_task_cancellation() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        let wrapperTask = Task {
+            // Wait until task is cancelled.
+            let didCancelStream = AsyncStream<Never> { _ in }
+            for await _ in didCancelStream { }
+            
+            await #expect(throws: CancellationError.self) {
+                try await queue.perform {
+                    Issue.record("Operation should not run")
+                }
+            }
+        }
+        
+        wrapperTask.cancel()
+        await wrapperTask.value
+    }
+    
+    @Test
+    func cancelled_task_does_not_wait_for_previous_operations() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        // GIVEN a first task that does not end until the test is complete.
+        let (firstTaskStream, firstTaskContinuation) = AsyncStream.makeStream(of: Never.self)
+        defer { firstTaskContinuation.finish() }
+        queue.addTask {
+            for await _ in firstTaskStream { }
+        }
+        
+        // GIVEN a discardable task enqueued after the first task.
+        let discardableTask = queue.addTask {
+            Issue.record("Operation should not run")
+        }
+        
+        // WHEN the discardable task is cancelled,
+        discardableTask.cancel()
+        
+        // THEN it completes with `CancellationError` before the first task has ended.
+        await #expect(throws: CancellationError.self) {
+            try await discardableTask.value
+        }
+    }
+    
+    @Test
+    func cancelled_operation_does_not_wait_for_previous_operations() async throws {
+        let queue = DiscardingAsyncQueue()
+        
+        let (firstTaskStream, firstTaskContinuation) = AsyncStream.makeStream(of: Never.self)
+        queue.addTask {
+            for await _ in firstTaskStream { }
+        }
+        
+        let wrapperTask = Task {
+            // Wait until task is cancelled.
+            let didCancelStream = AsyncStream<Never> { _ in }
+            for await _ in didCancelStream { }
+            
+            await #expect(throws: CancellationError.self) {
+                try await queue.perform {
+                    Issue.record("Operation should not run")
+                }
+            }
+            
+            firstTaskContinuation.finish()
+            
+            await #expect(throws: CancellationError.self) {
+                try await queue.perform {
+                    Issue.record("Operation should not run")
+                }
+            }
+        }
+        
+        wrapperTask.cancel()
+        await wrapperTask.value
+    }
+    
+    // MARK: - Isolation
+    
+    @Test
+    func perform_closure_inherits_isolation() {
+        // This test passes if it compiles without any error or warning.
+        @MainActor class MyClass {
+            let queue = DiscardingAsyncQueue()
+            
+            private func enqueue() async throws {
+                try await queue.perform {
+                    // Synchronous call is OK because the closure inherits
+                    // the current isolation.
+                    isolatedSynchronousMethod()
+                }
+            }
+            
+            private func isolatedSynchronousMethod() { }
+        }
+        
+        actor MyActor {
+            let queue = DiscardingAsyncQueue()
+            
+            private func enqueue() async throws {
+                try await queue.perform {
+                    // Synchronous call is OK because the closure inherits
+                    // the current isolation.
+                    isolatedSynchronousMethod()
+                }
+            }
+            
+            private func isolatedSynchronousMethod() { }
+        }
+    }
+    
+    @Test
+    func addTask_closure_inherits_isolation() {
+        // This test passes if it compiles without any error or warning.
+        @MainActor class MyClass {
+            let queue = DiscardingAsyncQueue()
+            
+            private func enqueue() -> Task<Void, any Error> {
+                queue.addTask {
+                    // Synchronous call is OK because the closure inherits
+                    // the current isolation.
+                    isolatedSynchronousMethod()
+                }
+            }
+            
+            private func isolatedSynchronousMethod() { }
+        }
+        
+        actor MyActor {
+            let queue = DiscardingAsyncQueue()
+            
+            private func enqueue() -> Task<Void, any Error> {
+                queue.addTask {
+                    // Synchronous call is OK because the closure inherits
+                    // the current isolation.
+                    isolatedSynchronousMethod()
+                }
+            }
+            
+            private func isolatedSynchronousMethod() { }
+        }
+    }
+}
